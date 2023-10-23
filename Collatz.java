e9f983e98db9aa1aa27af8a938ac705220711749
Sebastian Philipp <124450771+sebastianphilipp@users.noreply.github.com>
2023-10-23 21:47:45 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,20 +1,18 @@
 
 public class Collatz {
-
-	//Sebastian Philipp
 	
 	public static void main(String[] args) {
 
-		long zahl = In.readLong("Collatz’sche Zahlenfolge: Gebe eine Zahl >1 ein: ");
+		long zahl = In.readLong("Collatz Sequence: Enter a number greater than 1: ");
 		int zaehler = 0;
 		
 		if (zahl < 2) {
-			System.out.println("Fehler");
+			System.out.println("Error!");
 			System.exit(0);
 		} else {
 			while (zahl != 1) {
 				if (zaehler > 500) {
-					System.out.println("Mehr als 500 Durchgänge!");
+					System.out.println("More than 500 iterations!");
 					System.exit(0);
 				} else {
 					if (zahl % 2 == 0) {
@@ -30,7 +28,7 @@ public class Collatz {
 				}
 					
 			}
-			System.out.print("Ende, es waren so viele Durchgänge: " + zaehler);
+			System.out.print("End, there were so many iterations: " + zaehler);
 			
 		}
 		
