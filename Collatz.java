867ef3f70a7e515b9b84da8fa324e06d42b1abb7
Sebastian Philipp <124450771+sebastianphilipp@users.noreply.github.com>
2023-10-23 22:05:27 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,37 +1,42 @@
+import java.util.Scanner;
 
 public class Collatz {
-	
+
 	public static void main(String[] args) {
 
-		long zahl = In.readLong("Collatz Sequence: Enter a number greater than 1: ");
-		int zaehler = 0;
-		
-		if (zahl < 2) {
-			System.out.println("Error!");
-			System.exit(0);
-		} else {
-			while (zahl != 1) {
-				if (zaehler > 500) {
-					System.out.println("More than 500 iterations!");
-					System.exit(0);
-				} else {
-					if (zahl % 2 == 0) {
-						zahl /= 2;
-						System.out.println(zahl);
-						zaehler += 1;
+		try (Scanner scr = new Scanner(System.in)) {
+			System.out.print("Collatz Sequence: Enter a number greater than 1: ");
+			long zahl = scr.nextLong();
+
+			int zaehler = 0;
+
+			if (zahl < 2) {
+				System.out.println("Error!");
+				return;
+			} else {
+				while (zahl != 1) {
+					if (zaehler > 500) {
+						System.out.println("More than 500 iterations!");
+						System.exit(0);
 					} else {
-						zahl *= 3;
-						zahl++;
-						System.out.println(zahl);
-						zaehler += 1;
+						if (zahl % 2 == 0) {
+							zahl /= 2;
+							System.out.println(zahl);
+							zaehler += 1;
+						} else {
+							zahl *= 3;
+							zahl++;
+							System.out.println(zahl);
+							zaehler += 1;
+						}
 					}
+
 				}
-					
+				System.out.print("End, there were so many iterations: " + zaehler);
+
 			}
-			System.out.print("End, there were so many iterations: " + zaehler);
-			
 		}
-		
+
 	}
 
 }
